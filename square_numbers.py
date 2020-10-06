5d58056f14ec1c09fa42904d860392962d1bbdf5
CharalambosIoannou <cioannou1997@gmail.com>
2020-10-06 13:01:32 +0100

    added script to square numbers

---
+++ square_numbers.py
@@ -0,0 +1,5 @@
+# THE ONE-LINER
+nums_squared = list(map(lambda x: x * x, list(map(int,input("\nEnter the numbers to square : ").strip().split()))[:int(input("Enter how many numbers you have added: ")) ] ))
+
+# THE RESULT
+print(nums_squared)
